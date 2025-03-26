0b0d5784362172b76d240e0bed2824e86bb145c6
Abril Herbas <herbasabril4@gmail.com>
2025-03-26 12:13:46 -0300

    agregue una ruta que saluda con nombre

--- app.py
+++ app.py
@@ -3,5 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p> holaa!</p>"
+def principal():
+    return """
+     <a href= '/hola'>:)</a>" 
+     <a href= '/chau'>:(</a>" 
+     """
+
+@app.route("/hola")
+def saludar():
+    return f"<h2>hello!</h2>"
+
+@app.route("/hola/<string:nombre>")
+def saludar_con_nombre(nombre):
+    return f"<h2>hello,{nombre}!</h2>"
+
+@app.route("/chau")
+def despedir():
+    return "<h2>chau</h2>"
+
