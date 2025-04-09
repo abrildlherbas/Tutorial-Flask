4aab714ad7a5a9cb0e8aaf1f9b87ed3bcfdddc48
abril herbas <herbasabril4@gmail.com>
2025-04-09 12:02:23 -0300

    agregue el codigo de los links

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask,url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def principal():
+    url_hola = url_for("hello")
+    url_dado = url_for("dado",caras=6)
+    url_logo = url_for("static",filename="img/duki.png")
+    
     return """
      <a href= '/hola'>:)</a>" 
-     <a href= '/chau'>:(</a>" 
+     <a href= '/chau'>:(</a>"
+    <br>
+    <a href="{url_hola}">Hola</a>
+    <br>
+    <a href="{url_for("bye")}">chau</a>
+    <a href="{url_logo}">Logo</a>
+    <br>
+    <a href="{url_dado}">Tirar dado</a>
      """
 
 @app.route("/hola")
@@ -21,3 +32,17 @@ def saludar_con_nombre(nombre):
 def despedir():
     return "<h2>chau</h2>"
 
+
+def main():
+  url_hola = url_for("hello")
+  url_dado = url_for("dado",caras=6)
+  url_logo = url_for("static",filename="img/duki.png")
+ 
+  return f"""
+    <a href="{url_hola}">Hola</a>
+    <br>
+    <a href="{url_for("bye")}">chau</a>
+    <a href="{url_logo}">Logo</a>
+    <br>
+    <a href="{url_dado}">Tirar dado</a>
+ """
